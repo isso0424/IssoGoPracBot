c98459fde7e16310fd45a79792d87648c3543dc2
isso0424 <isso04240129@gmail.com>
2020-04-09 04:03:58 +0900

    [Add]OnReadyができるようになった

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -12,13 +14,17 @@ var (
 
 func main() {
 	discord, err := discordgo.New()
-	discord.Token = Token
+	token := loadTokenFromEnv()
+	if err != nil {
+		fmt.Println(err)
+	}
+	discord.Token = "Bot " + token
 	if err != nil {
 		fmt.Println("Error logging in")
 		fmt.Println(err)
 	}
 
-	discord.AddHandler(onMessageCreate)
+	discord.AddHandler(onReady)
 
 	err = discord.Open()
 	if err != nil {
@@ -27,6 +33,29 @@ func main() {
 	<-stopBot
 }
 
-func onMessageCreate(session *discordgo.Session, message *discordgo.MessageCreate) {
-	c, err := session.State.Channel(message.ChannelID)
+func onReady(session *discordgo.Session, ready *discordgo.Ready) {
+	sendMessage(session, "691261912008360000", "Hello world")
+}
+
+func sendMessage(session *discordgo.Session, channelID string, message string) {
+	_, err := session.ChannelMessageSend(channelID, message)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func loadTokenFromEnv() string {
+	fp, err := os.Open(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	defer fp.Close()
+
+	scanner := bufio.NewScanner(fp)
+	var token string
+	for scanner.Scan() {
+		token = scanner.Text()
+	}
+	return token
 }
